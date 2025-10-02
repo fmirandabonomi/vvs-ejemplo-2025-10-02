224d42ca99794623d546e08587cee106f3319d85
Fernando Alberto Miranda Bonomi <fmirandabonomi@herrera.unt.edu.ar>
2025-10-02 19:44:49 -0300

    Versión inicial - pasa prueba de suma

---
+++ test/nativo/test_suma/test_suma.c
@@ -0,0 +1,40 @@
+#include "suma.h"
+#include "unity.h"
+
+void setUp(void)
+{
+
+}
+void tearDown(void)
+{
+
+}
+
+static void test_debe_sumar_dos_enteros(void)
+{
+    {
+        const int a = 23;
+        const int b = 31;
+        const int valor_esperado = a + b;
+        
+        const int obtenido = suma(a,b);
+        
+        TEST_ASSERT_EQUAL_INT(valor_esperado, obtenido);
+    }
+    {
+        const int a = 17;
+        const int b = 11;
+        const int valor_esperado = a + b;
+        
+        const int obtenido = suma(a,b);
+        
+        TEST_ASSERT_EQUAL_INT(valor_esperado, obtenido);
+    }
+}
+
+int main(void)
+{
+    UNITY_BEGIN();
+    RUN_TEST(test_debe_sumar_dos_enteros);
+    return UNITY_END();
+}
