c3b09f8a9b4ce22e5fec76aac1b5c4b1ac7b3bed
Fernando Alberto Miranda Bonomi <fmirandabonomi@herrera.unt.edu.ar>
2025-10-02 19:49:29 -0300

    Documentación para API de suma

--- lib/suma/suma.h
+++ lib/suma/suma.h
@@ -1,6 +1,13 @@
 #ifndef SUMA_H
 #define SUMA_H
 
+/**
+ * @brief Suma dos enteros
+ * 
+ * @param a Primer entero
+ * @param b Segundo entero
+ * @return int La suma de a y b
+ */
 int suma(int a, int b);
 
 #endif
